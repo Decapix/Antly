247021875cdb86f6de34dd65886ab48181636cec
solenopsis <xxx@gmail.com>
2023-04-19 09:45:49 +0200

    https

--- src/LyAnt/settings.py
+++ src/LyAnt/settings.py
@@ -201,3 +201,9 @@ FREE_SHIPPING_COSTE = True
 # storage
 
 
+# Configuration pour utiliser SSL/TLS avec Heroku et Cloudflare (https)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
