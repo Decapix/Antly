30b965d082261e6f48119529c2edddfdd23039bd
solenopsis <xxx@gmail.com>
2023-04-27 16:58:54 +0200

    sitemap

--- src/LyAnt/sitemaps.py
+++ src/LyAnt/sitemaps.py
@@ -39,32 +39,12 @@ class PackSitemap(Sitemap):
         return reverse('pack_detail_n', args=[str(item.id)])
 
 
-class ShopperSitemap(Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
 
-    def items(self):
-        return Shopper_m.objects.all()
-
-    def location(self, item):
-        return reverse('dashboard_n', args=[str(item.id)])
-
-
-class OfferSitemap(Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
-
-    def items(self):
-        return Offer_m.objects.all()
-
-    def location(self, item):
-        return reverse('information_n', args=[str(item.id)])
 
 
 sitemaps = {
     'static': StaticSitemap,
     'ants': AntSitemap,
     'packs': PackSitemap,
-    'shoppers': ShopperSitemap,
-    'offers': OfferSitemap,
+
 }
