01c6651feb1fd12216fb88bc977fa1f9726be2f9
solenopsis <xxx@gmail.com>
2023-06-04 21:04:53 +0200

    superfeed

--- src/super/urls.py
+++ src/super/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', homepage_vi, name="homepage_n"),
     path('information/', information_vi, name="information_n"),
     path('information/cgv/', cgv_vi, name="cgv_n"),
+    path('superfeed.xml', superfeed_xml_view, name='superfeed_xml'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/super/views.py
+++ src/super/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from sale.models import *
 from user.models import Feedback_m
 from .models import *
-
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET
 
 def random_three_elements(input_list):
     if len(input_list) < 3:
@@ -48,3 +51,34 @@ def cgv_vi(request):
         "Conditions Générales de Vente | Boutique en Ligne de Fourmis Européennes pour Débutants",
         "Consultez les conditions générales de vente de notre boutique en ligne de fourmis européennes pour débutants. Découvrez nos politiques concernant les commandes, les prix, les paiements, les livraisons, les retours et la responsabilité, ainsi que des informations sur les données personnelles et le service clientèle.")
     return render(request, 'super/cgv.html', context={"meta": metat,})
+
+
+
+@require_GET
+@csrf_exempt
+def superfeed_xml_view(request):
+    # Retrieve all Ant_m, Pack_m, and Other_m products
+    ant_products = Ant_m.objects.filter(sizes__stock__gt=0).distinct()
+    pack_products = Pack_m.objects.filter(size__stock__gt=0).distinct()
+    other_products = Other_m.objects.filter(stock__gt=0).distinct()
+
+    # Load the template for the superfeed.xml file
+    template = loader.get_template('super/superfeed.xml')
+
+    # Context data to be passed to the template
+    context = {
+        'ant_products': ant_products,
+        'pack_products': pack_products,
+        'other_products': other_products,
+    }
+
+    # Render the template with the context data
+    rendered_template = template.render(context)
+
+    # Set the appropriate content type for the XML response
+    response = HttpResponse(content_type='application/xml')
+
+    # Set the content of the response to the rendered template
+    response.content = rendered_template
+
+    return response
