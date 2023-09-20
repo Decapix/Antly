3df47a0b17b78b1a52e626352059c751eed7feb6
solenopsis <xxx@gmail.com>
2023-09-20 17:06:55 +0200

    maj

--- src/sale/signals.py
+++ src/sale/signals.py
@@ -7,16 +7,6 @@ from django.dispatch import receiver
 
 
 
-#@receiver(post_save, sender=OrderTrack_m)
-#def send_tracking_email(sender, instance, **kwargs):
-#    order = instance.order
-#    latest_order_track = instance.order_track
-#    
-#    if order and order.owner:
-#        send_tracking_number_email(order, latest_order_track)
-
-
-
 @receiver(post_save, sender=OrderTrack_m)
 def send_tracking_number(sender, instance, created, **kwargs):
     if created:  # Vérifie si le numéro de suivi n'est pas vide
