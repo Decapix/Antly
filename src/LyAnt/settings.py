7f65a2f61a8a32d29734c664592d8659ecd2b579
solenopsis <xxx@gmail.com>
2023-12-24 09:47:07 +0100

    seller page

--- src/LyAnt/settings.py
+++ src/LyAnt/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('ENV') != 'PRODUCTION'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['antly.herokuapp.com', 'localhost', '127.0.0.1', 'antly.fr', 'www.antly.fr']
 
 
