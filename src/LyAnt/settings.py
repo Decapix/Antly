577cd699c2cac55867185369ff6d49314fb95487
Decapix <decapixd@gmail.com>
2024-04-22 12:33:18 +0200

    winter

--- src/LyAnt/settings.py
+++ src/LyAnt/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('ENV') != 'PRODUCTION'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['antly.fr', 'antly.herokuapp.com', 'localhost', '127.0.0.1', 'www.antly.fr']
 
 
@@ -259,4 +259,4 @@ CSRF_COOKIE_SECURE = True
 
 # winter
 
-WINTER = True
+WINTER = False
